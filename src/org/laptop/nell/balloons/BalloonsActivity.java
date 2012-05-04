420a9a92ef5763bf3f0488ad9f9b16f0cd815bd5
C. Scott Ananian <cscott@cscott.net>
2012-05-04 00:58:03 -0400

    Added PhoneGap framework.

--- src/org/laptop/nell/balloons/BalloonsActivity.java
+++ src/org/laptop/nell/balloons/BalloonsActivity.java
@@ -1,13 +1,13 @@
 package org.laptop.nell.balloons;
 
-import android.app.Activity;
 import android.os.Bundle;
+import org.apache.cordova.*;
 
-public class BalloonsActivity extends Activity {
+public class BalloonsActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }

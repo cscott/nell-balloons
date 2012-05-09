85f59b38168bc9dbef58d549e7497df714fc82fb
C. Scott Ananian <cscott@cscott.net>
2012-05-09 16:51:42 -0400

    Extend the Android/Honeycomb animation-end workaround to give us more padding.
    Apparently padding the animation by 2s wasn't enough.  Pad it by >10s.

--- assets/www/index.js
+++ assets/www/index.js
@@ -167,13 +167,6 @@ define(['domReady!', './alea', './buzz', './compat', './funf', 'nell!', 'score!'
                     var flex = document.querySelector('#foreground .award.flex');
                     flex.style.display = 'none';
                 }
-            } else if (isHoneycomb && this.popTimeout < 1000 &&
-                       this.domElement.classList.contains('squirt')) {
-                // work around a CSS animation bug in Android/Honeycomb
-                // (animation returns to start state after running)
-                this.domElement.style.WebkitTransform =
-                    'translate3d('+(-this.domElement.offsetWidth)+'px,'+
-                    (-this.domElement.offsetHeight)+'px,0)';
             }
             return;
         }

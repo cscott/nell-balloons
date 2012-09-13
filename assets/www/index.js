cscott/nell-balloons

commit 903abd7f774af5bf3b7ebccdf2ef83c2ffdaa9ea

Add dialog prompting to install as webapp on Firefox (if not already installed).

assets/www/index.js
@@ -749,13 +749,14 @@ define(['domReady!', './alea', './compat', './funf', 'nell!', 'score!', 'sound',
             underMode;
     };
     GameMode.OverlayMode.prototype.push = function() {
+        this.lastMode = GameMode.currentMode;
         this.setUnderMode(GameMode.currentMode.underMode ||
                           GameMode.currentMode);
         GameMode.switchTo(this);
     };
     GameMode.OverlayMode.prototype.pop = function() {
         console.assert(GameMode.currentMode === this);
-        GameMode.switchTo(this.underMode);
+        GameMode.switchTo(this.lastMode);
     };
     GameMode.OverlayMode.prototype.enter = (function(superEnter) {
         return function() {
@@ -926,6 +927,22 @@ define(['domReady!', './alea', './compat', './funf', 'nell!', 'score!', 'sound',
     };
 
     GameMode.Rotate = new GameMode.OverlayMode('rotate');
+
+    GameMode.Install = new GameMode.OverlayMode('install');
+    document.querySelector('#install .yes').addEventListener('click',function(){
+        GameMode.Install.maybeInstall(true);
+    }, false);
+    document.querySelector('#install .no').addEventListener('click',function(){
+        GameMode.Install.maybeInstall(false);
+    }, false);
+    GameMode.Install.maybeInstall = function(doInstall) {
+        var cb = function() { GameMode.Install.pop(); };
+        if (doInstall) {
+            GameMode.Install.doInstall(cb);
+        } else {
+            cb();
+        }
+    };
 
     GameMode.Playing = new GameMode('game');
     GameMode.Playing.toJSON = function() {
@@ -1388,6 +1405,43 @@ define(['domReady!', './alea', './compat', './funf', 'nell!', 'score!', 'sound',
                                  DOCUMENT_TITLE+' | Menu', '#menu');
             window.addEventListener('popstate', onPopState, false);
         }
+        // install webapp?
+        if (window && window.navigator &&
+            window.navigator.mozApps &&
+            window.navigator.mozApps.getSelf &&
+            window.navigator.mozApps.install) {
+            var request = window.navigator.mozApps.getSelf();
+            request.onsuccess = function() {
+                if (request.result) {
+                    /* we're already installed, do nothing */
+                    funf.record('installed', 'yes');
+                } else {
+                    /* not installed, prompt to install */
+                    funf.record('installed', 'no');
+                    GameMode.Install.doInstall = function(cb) {
+                        var request = window.navigator.mozApps.install(
+                            // XXX prefix not strictly required?
+                            'http://nell-balloons.github.cscott.net/'+
+                            'manifest.webapp');
+                        request.onsuccess = function() {
+                            funf.record('installed', 'success');
+                            cb(true);
+                        };
+                        request.onerror = function() {
+                            funf.record('installerror', this.error.name);
+                            cb(false);
+                        };
+                    };
+                    GameMode.Install.push();
+                }
+            };
+            request.onerror = function() {
+                funf.record('installerror', this.error.name);
+                console.log('Error checking installation status: ' +
+                            this.error.name);
+            };
+        }
+
         // orientation
         var isMobile = false;
         if (window.cordovaDetect) { isMobile = true; }

assets/www/index.html
@@ -185,6 +185,14 @@
  <div class="background"></div>
  <div class="foreground"></div>
 </div>
+<div id="install">
+  <p>
+  Would you like to install a shortcut to Nell's Balloons on your
+  home screen?
+  </p>
+  <button autofocus" class="yes">Yes</button>
+  <button class="no">No</button>
+</div>
   
 </body>
 </html>

assets/www/style.css
@@ -88,10 +88,10 @@ body { background: #008; }
 .video #video > .inner > a.arrow { pointer-events: all; }
 .video .nells > div > div { pointer-events: none; }
 
-#mask, #video, #menu, #rotate {
+#mask, #video, #menu, #rotate, #install {
   display: none;
 }
-.anim #mask, .anim #video, .anim #menu, .anim #rotate {
+.anim #mask, .anim #video, .anim #menu, .anim #rotate, .anim #install {
  /* transition effect knowledge gleaned from:
   *   http://www.greywyvern.com/?post=337
   * thanks! */
@@ -103,14 +103,18 @@ body { background: #008; }
  display: block;
 }
 .video #mask,
-.rotate #mask { opacity: 0.8; }
+.rotate #mask,
+.install #mask { opacity: 0.8; }
 .video #video,
 .menu #menu,
-.rotate #rotate { opacity: 1; }
+.rotate #rotate,
+.install #install { opacity: 1; }
 .video #mask,
 .video #video,
 .rotate #mask,
 .rotate #rotate,
+.install #mask,
+.install #install,
 .menu #menu {
   visibility: visible;
   /* change the visibility property immediately */
@@ -1579,3 +1583,22 @@ body { background: #008; }
      -moz-animation: rotate-throb 1s infinite alternate ease;
           animation: rotate-throb 1s infinite alternate ease;
 }
+
+/* ------- "install as webapp" dialog ------- */
+#install {
+  position: absolute;
+  width: 500px;
+  left: 50%; top: 16px;
+  margin-left: -250px;
+  background: white;
+  -webkit-border-radius: 13px;
+     -moz-border-radius: 13px;
+          border-radius: 13px;
+  -webkit-box-shadow: 0 0 7px 7px rgba(0,0,0,0.6);
+     -moz-box-shadow: 0 0 7px 7px rgba(0,0,0,0.6);
+          box-shadow: 0 0 7px 7px rgba(0,0,0,0.6);
+  border: 2px solid black;
+  padding: 8px;
+  text-align: center;
+}
+#install, #install button { font: 18px sans; }

assets/www/test.html
@@ -202,6 +202,14 @@ require([], function() {
  <div class="background"></div>
  <div class="foreground"></div>
 </div>
+<div id="install">
+  <p>
+  Would you like to install a shortcut to Nell's Balloons on your
+  home screen?
+  </p>
+  <button autofocus" class="yes">Yes</button>
+  <button class="no">No</button>
+</div>
 
 </body>
 </html>

commit fd09fa48577d9bac16e9db61b58e6482c595962a

Fix regression introduced by 903abd7f774af5bf3b7ebccdf2ef83c2ffdaa9ea.
We were popping() from the LevelDone mode right back to the video mode---not
the intended behavior!

assets/www/index.js
@@ -914,7 +914,8 @@ define(['domReady!', './alea', './compat', './funf', 'nell!', 'score!', 'sound',
                 sprout.setTime('0s', '1s');
             });
             // reset sound to match new level
-            return false;
+            GameMode.switchTo(GameMode.Playing);
+            return true;
         } else if (HTML5_HISTORY) {
             this.currentLevel = GameMode.Playing.currentLevel;
             history.back();